e94f3db3e0aa80769040dcbd1fdce1eed0265dff
shichaohui <464206200@qq.com>
2021-01-31 22:42:24 +0800

    feat commit queryer

---
+++ commit/config.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+# GitLab 地址 和 Access Token
+gitlabInfos = [
+    ('https://gitlab.***.com/', '********************'),
+]
+
+# 要查询的项目
+projectNames = [
+    'project-name-1',
+    'project-name-2',
+]
+
+# 要查询的用户名
+userNames = [
+    '石朝辉',
+    'shichaohui'
+]
+
+# 开始时间和结束时间
+startTime = '2020-01-01T00:00:00Z'
+endTime = '2020-12-31T23:59:59Z'
---
+++ commit/queryer.py
@@ -0,0 +1,81 @@
+# coding=utf-8
+
+import sys
+
+# 兼容 python2 的编码
+try:
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+except NameError as e:
+    pass
+
+# 导入线程库
+from threading import Thread, Lock
+
+# 导入 python-gitlab
+import gitlab
+
+# 导入配置
+from config import gitlabInfos, projectNames, userNames, startTime, endTime
+
+# 线程
+threads = []
+# 查询结果
+result = {}
+
+lock = Lock()
+
+# 查询指定 GitLab
+def inquireGitLab(url, token):
+    # 登录 获取gitlab操作对象gl
+    gl = gitlab.Gitlab(url, token)
+
+    # 先把所有项目查出来
+    projects = gl.projects.list(all=True)
+
+    # 遍历每一个项目
+    for project in projects:
+        if project.name in projectNames:
+            callback = lambda: inquireProject(project)
+            thread = Thread(target=callback)
+            threads.append(thread)
+            thread.start()
+
+# 查询指定项目
+def inquireProject(project):
+    commitIds = []
+    # 把每个项目下面的所有分支查出来
+    branches = project.branches.list()
+    # 然后再遍历每一个分支
+    for branch in branches:
+        # 根据时间、分支名遍历该分支下面所有的提交记录
+        params = { 'since': startTime, 'until': endTime, 'ref_name': branch.name }
+        commits = project.commits.list(all=True, query_parameters=params)
+        # 然后再遍历每个提交记录
+        for commit in commits:
+            # 去重
+            if commit.id in commitIds:
+                continue
+            # 去除 Merge
+            if commit.title.startswith('Merge'):
+                continue
+            name = commit.author_name
+            # 去除非指定用户
+            if (name not in userNames):
+                continue
+            lock.acquire()
+            result[name] = result.get(name, 0) + 1
+            lock.release()
+            commitIds.append(commit.id)
+            print(name, result[name])
+
+# 启动查询
+for info in gitlabInfos:
+    inquireGitLab(info[0], info[1])
+
+# 等待所有线程任务结束
+for t in threads:
+    t.join()
+
+print("result ===========>")
+print(result)
